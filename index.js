2f71ea7e6934963095028a4cafa196e18d9a33c3
Dhia' Alhaq Shalabi <dhia.shalabi@gmail.com>
2023-03-04 19:22:40 +0300

    Completed assignment.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName) {
+  const thankYouCards = [];
+
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    thankYouCards.push(message);
+  }
+
+  return thankYouCards;
+}
+
+function countDown(number) {
+  let i = number;
+  while (i >= 0) {
+    console.log(i);
+    i--;
+  }
+}
+
+countDown(10);
